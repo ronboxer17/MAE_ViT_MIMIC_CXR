9be3ac48bf8bb55ec5b654c6166c4969811ef3b8
itamar.fradkin <itamar.fradkin@explorium.ai>
2023-06-24 11:42:59 +0300

    add `display_image`

--- final_project/datasets/parking.py
+++ final_project/datasets/parking.py
@@ -1,7 +1,7 @@
 import os.path
 from typing import Tuple
 
-from torch.utils.data import random_split
+from torch.utils.data import random_split, DataLoader
 from torchvision import datasets, transforms
 
 from final_project.config import DATA_PATH
@@ -18,13 +18,13 @@ transformer = transforms.Compose(
 
 
 def load_parking_dataset(
-    root: str = data_root_path, transform=transformer
+        root: str = data_root_path, transform=transformer
 ) -> datasets.ImageFolder:
     return datasets.ImageFolder(root=root, transform=transform)
 
 
 def train_test_mock_data(
-    train_ratio=0.8,
+        train_ratio=0.8,
 ) -> Tuple[datasets.ImageFolder, datasets.ImageFolder]:
     dataset = load_parking_dataset()
     train_size = int(train_ratio * len(dataset))
@@ -32,3 +32,14 @@ def train_test_mock_data(
         dataset, [train_size, len(dataset) - train_size]
     )
     return train_dataset, val_dataset
+
+
+if __name__ == "__main__":
+    from final_project.utils import display_image
+
+    data, _ = train_test_mock_data()
+    dataloader = DataLoader(data, batch_size=1)
+    for batch in dataloader:
+        print(batch)
+        print()
+        break
---
+++ final_project/utils.py
@@ -0,0 +1,27 @@
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def display_image(image_tensor: torch.Tensor):
+    # Convert tensor to numpy array
+    image_np = image_tensor.numpy()
+
+    # Check if the image tensor was on a CUDA device, and if so move it back to CPU
+    if torch.cuda.is_available():
+        image_np = image_tensor.cpu().numpy()
+
+    # PyTorch tensors for images have the shape (C, H, W) so we need to transpose it to (H, W, C)
+    image_np = np.transpose(image_np, (1, 2, 0))
+
+    # If the image's pixel values range from 0-255, normalize to 0-1
+    if np.max(image_np) > 1:
+        image_np = image_np / 255
+
+    # Display the image
+    plt.imshow(image_np)
+    plt.axis('off')  # Turn off axis numbers and labels
+    plt.show()
+
+# To use the function:
+# display_image(your_image_tensor)
