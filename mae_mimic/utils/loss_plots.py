e326ecaa82103143244fdfd42690769221209002
Ron Boxer <88500712+ronboxer17@users.noreply.github.com>
2023-09-17 18:05:11 +0300

    Create loss_plots.py
    Add Loss plots

---
+++ mae_mimic/utils/loss_plots.py
@@ -0,0 +1,68 @@
+import os
+import json
+import matplotlib.pyplot as plt
+
+
+def calculate_mean_losses(file_path: str):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    transformer_name = data['metadata']['transformer']
+    sample_size = data['metadata']['sample_size']
+    results = data['results']
+
+    epoch_numbers = []
+    mean_losses = []
+
+    max_epochs = 50 if sample_size == 1000 else 20
+
+    for epoch_key in results.keys():
+        if int(epoch_key) < max_epochs:
+            epoch_data = results[epoch_key]
+            losses = epoch_data.get('losses', [])
+            if losses:
+                mean_loss = sum(losses) / len(losses)
+                epoch_numbers.append(int(epoch_key))
+                mean_losses.append(mean_loss)
+
+    return sample_size, transformer_name, epoch_numbers, mean_losses
+
+
+def plot_losses(file_path: str):
+    data_by_sample_size = {}
+
+    # Step 2: Iterate over all JSON files in the directory
+    for root, dirs, files in os.walk(file_path):
+        for file in files:
+            if file.endswith(".json"):
+                file_path = os.path.join(root, file)
+
+                # Step 3: Extract sample_size and categorize the file
+                sample_size, transformer_name, epoch_numbers, mean_losses = calculate_mean_losses(file_path)
+
+                if sample_size not in data_by_sample_size:
+                    data_by_sample_size[sample_size] = []
+
+                data_by_sample_size[sample_size].append((transformer_name, epoch_numbers, mean_losses))
+
+    # Step 5: Create two separate plots for each sample_size
+    for sample_size, data_list in data_by_sample_size.items():
+        plt.figure()
+        for data in data_list:
+            transformer_name, epoch_numbers, mean_losses = data
+            plt.plot(epoch_numbers, mean_losses, marker='o', label=transformer_name)
+
+        plt.xlabel('Epoch')
+        plt.ylabel('Mean Loss')
+        plt.title(f'Mean Loss per Epoch (Sample Size: {sample_size})')
+        plt.grid(True)
+        plt.legend()
+
+        if sample_size == 10000:
+            plt.xlim(0, 20)  # Set x-axis limit to 0-20 for sample_size=10000
+
+        plt.show()
+
+
+directory = r"../assets/metrics"
+plot_losses(directory)
